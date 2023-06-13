00e8636bb2dac523b90a92e59880c621cbd4b82a
Sathvik Saran Atchukolu <94753192+sathviksaran@users.noreply.github.com>
2023-06-13 23:35:01 +0530

    Update Student Registration System.py

--- Student Registration System.py
+++ Student Registration System.py
@@ -15,34 +15,12 @@ framefg="#06283D"
 
 root=Tk()
 root.title("Student Registration System")
-root.geometry("1250x750")
+root.geometry("1050x710")
 root.config(bg=background)
+root.resizable(False,False)
 
 
-file=pathlib.Path('Student_data.xlsx')
-if file.exists():
-    pass
-else:
-    file=Workbook()
-    sheet=file.active
-    sheet['A1']="Registration No."
-    sheet['B1']="Name"
-    sheet['C1']="Class"
-    sheet['D1']="Gender"
-    sheet['E1']="DOB"
-    sheet['F1']="Date of Registration"
-    sheet['G1']="Religion"
-    sheet['H1']="Skill"
-    sheet['I1']="Father Name"
-    sheet['J1']="Mother Name"
-    sheet['K1']="Father's Occupation"
-    sheet['L1']="Mother's Occupation"
 
-    file.save('Student_data.xlsx')
-
-###########Exit window########
-def Exit():
-    root.destroy()
 
 ###########ShowImage##########
 def showimage():
@@ -58,21 +36,6 @@ def showimage():
     lbl.config(image=photo2)
     lbl.image=photo2
 
-###########Registration No.#####
-#now each time we have to enter registration no.
-#lets design automatic registration no. entry system
-
-def registration_no():
-    file=openpyxl.load_workbook('Student_data.xlsx')
-    sheet=file.active
-    row=sheet.max_row
-
-    max_row_value=sheet.cell(row=row,column=1).value
-
-    try:
-        Registration.set(max_row_value+1)
-    except:
-        Registration.set("1")
 
 ##########Clear###########
 def Clear():
@@ -280,51 +243,18 @@ def selection():
     else:
         gender="Others"
 
-#top frames
-Label(root,text="Email: atchukolus@gmail.com",width=10,height=3,bg="#f0687c",anchor='e').pack(side=TOP,fill=X)
-Label(root,text="STUDENT REGISTRATION",width=10,height=2,bg="#c36464",fg="#fff",font='arial 20 bold').pack(side=TOP,fill=X)
-
-#search box to update
-Search=StringVar()
-Entry(root,textvariable=Search,width=15,bd=2,font="arial 20").place(x=1020,y=70)
-imageicon3=PhotoImage(file="Images/search.png")
-Srch=Button(root,text="Search",compound=LEFT,image=imageicon3,width=123,bg='#68ddfa',font="arial 13 bold",command=search)
-Srch.place(x=1260,y=66)
-
-imageicon4=PhotoImage(file="Images/Layer 4.png")
-Update_button=Button(root,image=imageicon4,bg="#c36464",command=Update)
-Update_button.place(x=110,y=64)
-
-#Registration and Date
-Label(root,text="Registration No:",font="arial 13",fg=framebg,bg=background).place(x=30,y=150)
-Label(root,text="Date:",font="arial 13",fg=framebg,bg=background).place(x=500,y=150)
-
-Registration=IntVar()
-Date=StringVar()
-
-reg_entry=Entry(root,textvariable=Registration,width=15,font="arial 10")
-reg_entry.place(x=160,y=150)
-
-registration_no()
-
-today=date.today()
-d1=today.strftime("%d/%m/%Y")
-date_entry=Entry(root,textvariable=Date,width=15,font="arial 10")
-date_entry.place(x=550,y=150)
-
-Date.set(d1)
 
 #Student details
-obj=LabelFrame(root,text="Student's Details",font=20,bd=2,width=900,bg=framebg,fg=framefg,height=250,relief=GROOVE)
-obj.place(x=30,y=200)
+obj=LabelFrame(root,text="Student's Details",font=20,bd=2,width=990,bg=framebg,fg=framefg,height=650,relief=GROOVE)
+obj.place(x=30,y=30)
 
 Label(obj,text="Full Name:",font="arial 13",bg=framebg,fg=framefg).place(x=30,y=50)
 Label(obj,text="Date of Birth:",font="arial 13",bg=framebg,fg=framefg).place(x=30,y=100)
 Label(obj,text="Gender:",font="arial 13",bg=framebg,fg=framefg).place(x=30,y=150)
 
-Label(obj,text="Class:",font="arial 13",bg=framebg,fg=framefg).place(x=500,y=50)
-Label(obj,text="Religion:",font="arial 13",bg=framebg,fg=framefg).place(x=500,y=100)
-Label(obj,text="Skills:",font="arial 13",bg=framebg,fg=framefg).place(x=500,y=150)
+Label(obj,text="Class:",font="arial 13",bg=framebg,fg=framefg).place(x=400,y=50)
+Label(obj,text="Religion:",font="arial 13",bg=framebg,fg=framefg).place(x=400,y=100)
+Label(obj,text="Skills:",font="arial 13",bg=framebg,fg=framefg).place(x=400,y=150)
 
 Name=StringVar()
 name_entry=Entry(obj,textvariable=Name,width=20,font="arial 10")
@@ -346,61 +276,26 @@ R3.place(x=260,y=150)
 
 Religion=StringVar()
 religion_entry=Entry(obj,textvariable=Religion,width=20,font="arial 10")
-religion_entry.place(x=630,y=100)
+religion_entry.place(x=530,y=100)
 
 Skill=StringVar()
 skill_entry=Entry(obj,textvariable=Skill,width=20,font="arial 10")
-skill_entry.place(x=630,y=150)
+skill_entry.place(x=530,y=150)
 
 Class=Combobox(obj,values=['1','2','3','4','5','6','7','8','9','10','11','12'],font="Roboto 10",width=17,state="r")
-Class.place(x=630,y=50)
+Class.place(x=530,y=50)
 Class.set("Select Class")
 
 
-#Parent details
-obj2=LabelFrame(root,text="Parent's Details",font=20,bd=2,width=900,bg=framebg,fg=framefg,height=250,relief=GROOVE)
-obj2.place(x=30,y=470)
-
-Label(obj2,text="Father's Name:",font="arial 13",bg=framebg,fg=framefg).place(x=30,y=50)
-Label(obj2,text="Occupation:",font="arial 13",bg=framebg,fg=framefg).place(x=30,y=100)
-
-F_Name=StringVar()
-f_entry=Entry(obj2,textvariable=F_Name,width=20,font="arial 10")
-f_entry.place(x=160,y=50)
-
-Father_Occupation=StringVar()
-fo_entry=Entry(obj2,textvariable=Father_Occupation,width=20,font="arial 10")
-fo_entry.place(x=160,y=100)
-
-Label(obj2,text="Mother's Name:",font="arial 13",bg=framebg,fg=framefg).place(x=500,y=50)
-Label(obj2,text="Occupation:",font="arial 13",bg=framebg,fg=framefg).place(x=500,y=100)
-
-M_Name=StringVar()
-m_entry=Entry(obj2,textvariable=M_Name,width=20,font="arial 10")
-m_entry.place(x=630,y=50)
-
-Mother_Occupation=StringVar()
-mo_entry=Entry(obj2,textvariable=Mother_Occupation,width=20,font="arial 10")
-mo_entry.place(x=630,y=100)
-
 #image
 f=Frame(root,bd=3,bg="black",width=200,height=200,relief=GROOVE)
-f.place(x=1200,y=150)
+f.place(x=800,y=80)
 
 img=PhotoImage(file="Images/upload photo.png")
 lbl=Label(f,bg="black",image=img)
 lbl.place(x=0,y=0)
 
 #button
-Button(root,text="Upload",width=19,height=2,font="arial 12 bold",bg="lightblue",command=showimage).place(x=1200,y=370)
-
-saveButton=Button(root,text="Save",width=19,height=2,font="arial 12 bold",bg="lightgreen",command=Save)
-saveButton.place(x=1200,y=450)
-
-Button(root,text="Remove",width=19,height=2,font="arial 12 bold",bg="red",command=delete).place(x=1200,y=530)
-
-Button(root,text="Reset",width=19,height=2,font="arial 12 bold",bg="lightpink",command=Clear).place(x=1200,y=610)
-
-Button(root,text="Exit",width=19,height=2,font="arial 12 bold",bg="grey",command=Exit).place(x=1200,y=690)
+Button(root,text="Upload",width=19,height=2,font="arial 12 bold",bg="lightblue",command=showimage).place(x=800,y=300)
 
 root.mainloop()
